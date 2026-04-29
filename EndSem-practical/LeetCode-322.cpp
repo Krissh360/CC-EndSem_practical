9a0f0202855a8232cdc6b348d42fdfbdc1e84e5e
Krissh Chhabra <krisshchhabra77@gmail.com>
2026-04-29 12:18:49 +0530

    Add files via upload
    commit source files and submission screenshots for end sem practical

---
+++ EndSem-practical/LeetCode-322.cpp
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int coinChange(vector<int>& coins, int amount) {
+
+        vector<int> dp(amount+1, INT_MAX);
+        dp[0] = 0;
+
+        for (int c : coins)
+        {
+            for (int i = c; i <= amount; i++)
+            {
+                if (dp[i-c] == INT_MAX) continue;
+                dp[i] = min(dp[i], dp[i-c]+1);
+            }
+        }
+
+        return dp[amount] == INT_MAX ? -1 : dp[amount];
+
+    }
+};
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int n, amount;
+    cin >> n >> amount;
+
+    vector<int> coins(n);
+    for (int i = 0; i < n; i++) {
+        cin >> coins[i];
+    }
+
+    Solution sol;
+    int ans = sol.coinChange(coins, amount);
+    cout << ans << "\n";
+
+    return 0;
+}
---
+++ EndSem-practical/prefix-sum-queries.cpp
@@ -0,0 +1,55 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+  public:
+    vector<int> rangeSumQueries(vector<int>& arr, vector<vector<int>>& queries) {
+        
+        int n = arr.size();
+        vector<int> prefix(n), ans;
+        
+        prefix[0] = arr[0];
+        
+        for (int i = 1; i < n; i++)
+        {
+            prefix[i] = prefix[i - 1] + arr[i];
+        }
+        
+        for (auto &q : queries)
+        {
+            int l = q[0], r = q[1];
+            if (l == 0) ans.push_back(prefix[r]);
+            else ans.push_back(prefix[r] - prefix[l - 1]);
+        }
+        
+        return ans;
+        
+    }
+};
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int n, m;
+    cin >> n >> m;
+
+    vector<int> arr(n);
+    for (int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
+
+    vector<vector<int>> queries(m, vector<int>(2));
+    for (int i = 0; i < m; i++) {
+        cin >> queries[i][0] >> queries[i][1];
+    }
+
+    Solution sol;
+    vector<int> ans = sol.rangeSumQueries(arr, queries);
+
+    for (int x : ans) {
+        cout << x << "\n";
+    }
+
+    return 0;
+}
